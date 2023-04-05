0d731b72763db295132a9095c2b743f5a7a37cd5
Krzysztof Halwa <krzysztof.halwa@solarwinds.com>
2023-04-05 16:31:20 +0200

    Update PlayersList.tsx

--- src/components/playersList/PlayersList.tsx
+++ src/components/playersList/PlayersList.tsx
@@ -4,18 +4,22 @@ import "./PlayersList.css";
 export function PlayersList() {
     return (
         <table id="players">
-            <tr>
-                <th>Zawodnik</th>
-                <th>Pozycja</th>
-                <th>Obecny klub</th>
-                <th>Wartość na rynku transferowym</th>
-                <th>Opcje</th>
-            </tr>
-            <tr>
-                <td colSpan={5}>
-                    Brak zawodników do wyświetlenia.
-                </td>
-            </tr>
+            <thead>
+                <tr>
+                    <th>Zawodnik</th>
+                    <th>Pozycja</th>
+                    <th>Obecny klub</th>
+                    <th>Wartość na rynku transferowym</th>
+                    <th>Opcje</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td colSpan={5}>
+                        Brak zawodników do wyświetlenia.
+                    </td>
+                </tr>
+            </tbody>
         </table>
     )
 }
