7fa9f3b541db10a35df5780feca6592bff582353
Krzysztof Halwa <krzysztof.halwa@solarwinds.com>
2023-04-05 09:37:49 +0200

    Added CSS style.

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import "./Welcome.css";
 
 export function Welcome() {
     return (
-        <h1>Witaj Fernando Santos!</h1>
+        <h1 className="welcome-header">Witaj Fernando Santos!</h1>
     )
 }
